e3268f7ab3bc3f770fb2f0a2c0692cb5b1da1fa8
rin0sdr <125930612+rin0sdr@users.noreply.github.com>
2023-02-20 23:56:27 +0100

    Add files via upload

---
+++ lab1_web.py
@@ -0,0 +1,20 @@
+import requests
+url = 'https://newton.vercel.app/api/v2/derive/x^2-2x'
+response = requests.get(url)
+
+# Result
+data = response.json()
+result = data['result']
+
+print(result) # prints "2x-2"
+
+# Create a web page in Python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return result
+if __name__ == "__main__":
+    app.run()
