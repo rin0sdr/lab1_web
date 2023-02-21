d942c3030186b7c0dc851ad92dfbdda0f13e3c1a
rin0sdr <125930612+rin0sdr@users.noreply.github.com>
2023-02-21 01:10:18 +0100

    Update lab1_web.py

--- lab1_web.py
+++ lab1_web.py
@@ -1,18 +1,11 @@
 import requests
 url = 'https://newton.vercel.app/api/v2/derive/x^2-2x'
 response = requests.get(url)
-
-# Result
 data = response.json()
 result = data['result']
-
-print(result) # prints "2x-2"
-
-#web page in Python
+print(result)
 from flask import Flask
-
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return result
